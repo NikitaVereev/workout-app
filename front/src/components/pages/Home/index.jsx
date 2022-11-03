48f01d5f0240a2ef720a0e722e5ce577b2e830d4
Nikita <nikitkavershinin2019@gmail.com>
2022-11-03 09:51:43 +0300

    callback, newPage, imageHeight

--- front/src/components/pages/Home/index.jsx
+++ front/src/components/pages/Home/index.jsx
@@ -4,11 +4,24 @@ import Button from '../../ui/Button/Button'
 import Counters from '../../ui/Counters'
 import styles from './Home.module.sass'
 import bgImage from '../../images/bg1.jpg'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+	const history = useNavigate()
 	return (
-		<Layout className={styles.container} height='100%' bgImage={bgImage}>
-			<Button style='main' text={'New'} callback={() => {}} />
+		<Layout
+			className={styles.container}
+			bgImage={bgImage}
+			height='calc(100vh - 60px)'
+		>
+			<Button
+				style='main'
+				text={'New'}
+				callback={() => {
+					history('/newpage')
+				}}
+			/>
+
 			<h1 className={styles.title}>EXERCISES FOR THE SHOULDERS</h1>
 			<Counters />
 		</Layout>
